dc1f75a2c09808141010137c53260245b2dcb265
DerBaumann <natur5210@gmail.com>
2026-05-04 15:45:02 +0200

    Add Place entity

---
+++ src/main/java/ch/wegmann/pierre/order_manager/place/Place.java
@@ -0,0 +1,31 @@
+package ch.wegmann.pierre.order_manager.place;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Size;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.NonNull;
+import lombok.RequiredArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@RequiredArgsConstructor
+@Entity
+@Table(name = "places")
+public class Place {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    @NonNull
+    private String name;
+
+    @Column(name = "post_code", nullable = false, length = 4)
+    @NonNull
+    private String postCode;
+
+    @Column(nullable = false)
+    @NonNull
+    private String canton;
+}
