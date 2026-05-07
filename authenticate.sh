1e2e6e82849441d303b8b857553150a5a67c4c3a
DerBaumann <natur5210@gmail.com>
2026-05-07 08:49:46 +0200

    Add more users for testing and fix db connection in application.yaml

--- authenticate.sh
+++ authenticate.sh
@@ -1,7 +1,29 @@
 #!/usr/bin/env bash
 
-TOKEN=$(http --form POST http://localhost:8080/realms/order_manager/protocol/openid-connect/token \
+# No Roles
+export TOKEN=$(http --form POST http://localhost:8080/realms/order_manager/protocol/openid-connect/token \
   client_id=order_manager_api \
   username=test \
+  password=test \
+  grant_type=password | jq -r .access_token)
+
+# Read
+export READ_TOKEN=$(http --form POST http://localhost:8080/realms/order_manager/protocol/openid-connect/token \
+  client_id=order_manager_api \
+  username=read \
+  password=test \
+  grant_type=password | jq -r .access_token)
+
+# Update
+export UPDATE_TOKEN=$(http --form POST http://localhost:8080/realms/order_manager/protocol/openid-connect/token \
+  client_id=order_manager_api \
+  username=update \
+  password=test \
+  grant_type=password | jq -r .access_token)
+
+# Admin
+export ADMIN_TOKEN=$(http --form POST http://localhost:8080/realms/order_manager/protocol/openid-connect/token \
+  client_id=order_manager_api \
+  username=test_admin \
   password=test \
   grant_type=password | jq -r .access_token)
--- src/main/java/ch/wegmann/pierre/order_manager/place/PlaceController.java
+++ src/main/java/ch/wegmann/pierre/order_manager/place/PlaceController.java
@@ -1,5 +1,8 @@
 package ch.wegmann.pierre.order_manager.place;
 
+import ch.wegmann.pierre.order_manager.security.Roles;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import jakarta.annotation.security.RolesAllowed;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -9,17 +12,20 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/places")
+@SecurityRequirement(name = "bearerAuth")
 @RequiredArgsConstructor
 public class PlaceController {
     private final PlaceService placeService;
 
     @GetMapping("/")
+    @RolesAllowed({Roles.Read, Roles.Update, Roles.Admin})
     public @ResponseBody List<Place> findAll() {
         return placeService.findAll();
     }
 
     // TODO: Cleaner solution to handle missing value
     @GetMapping("/{id}")
+    @RolesAllowed({Roles.Read, Roles.Update, Roles.Admin})
     public ResponseEntity<Place> findOne(@PathVariable int id) {
         return placeService
                 .find(id)
