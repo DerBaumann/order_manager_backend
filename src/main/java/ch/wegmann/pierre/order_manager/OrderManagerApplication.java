51e300295baa3c25ceb4ecc0e5595ff9be85568f
DerBaumann <natur5210@gmail.com>
2026-05-08 08:56:26 +0200

    Adjust schema.aml to match current entity setup

--- src/main/java/ch/wegmann/pierre/order_manager/OrderManagerApplication.java
+++ src/main/java/ch/wegmann/pierre/order_manager/OrderManagerApplication.java
@@ -6,7 +6,6 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 // TODO: Proper handling of EntityNotFoundException
 // TODO: DTO's for Order and maybe OrderPosition
-// TODO: Adjust schema.aml to match current entity setup
 
 @SpringBootApplication
 @EnableJpaRepositories
